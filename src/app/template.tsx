04758db7ff412308e9638ac0a9e432897d67103e
AQuoc98 <badguykendf@gmail.com>
2024-10-08 10:09:56 +0700

    feat: turn off scroll progress

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import useScrollProgress from "@/hooks/useScrollProgress";
 import { motion } from "framer-motion";
 import { ReactNode } from "react";
 
@@ -10,22 +9,15 @@ const variants = {
 };
 
 const Template = ({ children }: { children: ReactNode }) => {
-  const completion = useScrollProgress();
   return (
-    <>
-      <motion.main
-        variants={variants}
-        initial="hidden"
-        animate="enter"
-        transition={{ type: "linear", delay: 0.2, duration: 0.4 }}
-      >
-        {children}
-      </motion.main>
-      <span
-        style={{ transform: `translateY(${completion - 100}%)` }}
-        className="fixed top-0 bottom-0 right-0 z-50 w-1 transition-all duration-700 bg-primary"
-      ></span>
-    </>
+    <motion.main
+      variants={variants}
+      initial="hidden"
+      animate="enter"
+      transition={{ type: "linear", delay: 0.2, duration: 0.4 }}
+    >
+      {children}
+    </motion.main>
   );
 };
 
