531ea0d22694b3da889cf8094b3c49df3b9eb304
AQuoc98 <badguykendf@gmail.com>
2024-10-14 18:04:53 +0700

    feat: add footer section

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,21 @@
+import Socials from "./socials";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="py-12">
+      <div className="container mx-auto">
+        <div className="flex flex-col items-center justify-between">
+          <Socials
+            containerStyles="flex gap-x-6 mx-auto xl:mx-0 mb-4"
+            iconsStyles="text-xl"
+          />
+          <div className="text-muted-foreground">
+            Copyright &copy; Ken. All rights reserved.
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
