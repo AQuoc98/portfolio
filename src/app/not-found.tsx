b1b0b7e5a0c76f125dc4ef468f37d0d6ea56c2cd
AQuoc98 <badguykendf@gmail.com>
2024-10-28 00:31:05 +0700

    feat: add not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <section className="h-screen flex items-center justify-center">
+      <h1 className="text-5xl">Page Not Found</h1>
+    </section>
+  );
+};
+
+export default NotFound;
